2d6e4f296f374a1741479aabc5593c44851e2019
paulo.campos <paulorgcampos@gmail.com>
2022-12-04 22:14:44 +0000

    add challenge

--- main.go
+++ main.go
@@ -2,17 +2,39 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	fmt.Println(s)
 }
 
 func main() {
-	go printSomething("This is the first thing to be printed!")
+	challenge1()
+	return
+	var wg sync.WaitGroup
 
-	time.Sleep(1 * time.Second)
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"zeta",
+		"eta",
+		"theta",
+		"epsilon",
+	}
 
-	printSomething("This is the second thing to be printed!")
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+	wg.Add(1)
+	printSomething("This is the second thing to be printed!", &wg)
 }
